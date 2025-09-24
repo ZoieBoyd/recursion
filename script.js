aae7fe80a2ace7b6a0ff85fd547134a73119a6f3
ZoieBoyd <zoieboyd@outlook.com>
2025-09-24 17:52:13 +0100

    Create functions to return fibonacci sequence
    Created an iterative function and a recursive function that returns an array of a specified length of the Fibonacci sequence

---
+++ script.js
@@ -0,0 +1,20 @@
+// Iterative
+function fib(n) {
+   let seq = [0, 1];
+   if (n <= 2) return seq.slice(0, n);
+
+   for (let i = 2; i < n; i++) {
+      seq.push(seq[i - 1] + seq[i - 2]);
+   }
+   return seq;
+}
+
+// Recursive
+function fibRec(n) {
+   if (n === 1) return [0];
+   if (n === 2) return [0, 1];
+
+   const seq = fibRec(n - 1);
+   seq.push(seq[seq.length - 1] + seq[seq.length - 2]);
+   return seq;
+}
