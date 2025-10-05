bab39d811fc3200494cedd7365e090c83ae0c945
ZoieBoyd <zoieboyd@outlook.com>
2025-10-05 20:22:14 +0100

    Create function to return a merge sorted array
    Created functions which recursively sort a given array using the merge sort algorithm

---
+++ mergeSort.js
@@ -0,0 +1,18 @@
+function mergeSort(arr) {
+   if (arr.length <= 1) {
+      return arr;
+   }
+   const mid = arr.length / 2;
+   const left = arr.slice(0, mid);
+   const right = arr.slice(mid, arr.length);
+   return merge(mergeSort(left), mergeSort(right));
+}
+
+function merge(left, right) {
+   const mergedArr = [];
+   while (left.length && right.length) {
+      const smallest = left[0] < right[0] ? left.shift() : right.shift();
+      mergedArr.push(smallest);
+   }
+   return [...mergedArr, ...left, ...right];
+}
